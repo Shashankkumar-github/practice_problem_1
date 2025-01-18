a7045763c80d8228f20e7d6803557d7bfdf02046
Shashankkumar-github <101034283+Shashankkumar-github@users.noreply.github.com>
2025-01-18 15:24:21 +0530

    read

---
+++ read.java
@@ -0,0 +1,28 @@
+class read{ 
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+int n = sc.nextInt();
+switch(n){
+case 1 :System.out.print("one");
+break;
+case 2 :System.out.printI("two");
+break;
+case 3: System.out.print("three");
+break;
+case 4 :System.out.print("four");
+break;
+case 5 :System.out.print("five");
+break:
+case 6 : Systewm.out.print("six");
+break;
+case 7 : System.out.print("seven");
+break;
+case 8:System.ou.print("eight");
+break;
+case 9: System.out.print("nine");
+break;
+}
+}
+}
+
+
